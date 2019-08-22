48d2f8babab63386744934601511cc02200973d6
Henrique <henrique.malone@gmail.com>
2019-08-22 11:18:21 -0300

    Atualizações no Perfil/Menu -Recupera dados para preencher o perfil -Atualiza dados ao clicar em Salvar -Menu corrigido

--- app/src/main/java/com/example/si700_imobiliaria/Perfil.java
+++ app/src/main/java/com/example/si700_imobiliaria/Perfil.java
@@ -31,9 +31,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,11 +51,16 @@ public class Perfil extends AppCompatActivity
     private EditText edtTel;
     private EditText edtCel;
     private EditText edtEmail;
-
+    private Button btnSalvar;
 
     private FirebaseAuth firebaseauth;
     private DatabaseReference firebasereference = FirebaseDatabase.getInstance().getReference();
     private DatabaseReference usuarioReferencia = firebasereference.child("usuarios");
+
+    final String[] nome = new String[1];
+    final String[] sobrenome = new String[1];
+    final String[] email = new String[1];
+    final String[] senha = new String[1];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +68,13 @@ public class Perfil extends AppCompatActivity
         setContentView(R.layout.activity_perfil);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
 
         nomeSobr = findViewById(R.id.textNome);
         rbCliente = findViewById(R.id.rbCliente);
@@ -70,18 +84,26 @@ public class Perfil extends AppCompatActivity
         edtTel = findViewById(R.id.edtTel);
         edtCel = findViewById(R.id.edtCel);
         edtEmail = findViewById(R.id.edtEmail);
+        btnSalvar = findViewById(R.id.btnSalvar);
 
         firebaseauth = FirebaseAuth.getInstance();
 
-        onStart();
+        loadData(); //carrega dados do usuário
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(this);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkPhones() == true){ //se os campos de telefone e celular estiverem preenchidos
+                    if(updateData() == true){ //se a atualização dos dados ocorrer bem
+                        Toast.makeText(getApplicationContext(),"Registro atualizado com sucesso",Toast.LENGTH_LONG).show();
+                    } else{
+                        Toast.makeText(getApplicationContext(),"Erro ao atualizar o registro",Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(),"Há campos não preenchidos",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -134,7 +156,6 @@ public class Perfil extends AppCompatActivity
             Log.i("logOut", "saiu");
             firebaseauth = FirebaseAuth.getInstance();
             firebaseauth.signOut();
-            onDestroy();
             startActivity(new Intent(getApplicationContext(), MainActivity.class)); //retorna para tela de login
 
             return true;
@@ -147,6 +168,7 @@ public class Perfil extends AppCompatActivity
         return true;
     }
 
+    //Exibir nome do usuário no menu
     public void setUserName(String nome){
         // Obtém a referência do layout de navegação
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -159,6 +181,7 @@ public class Perfil extends AppCompatActivity
         txtNomeUser.setText("Olá, " +nome);
     }
 
+    //Exibir email do usuário no menu
     public void setUserEmail(String email){
         // Obtém a referência do layout de navegação
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -171,12 +194,17 @@ public class Perfil extends AppCompatActivity
         txtEmailUser.setText(email);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        final String[] nome = new String[1];
-        final String[] email = new String[1];
+    //Verifica se os campos telefone e celular estão preenchidos
+    public boolean checkPhones(){
+        if(edtTel.getText().toString().equals("") || edtCel.getText().toString().equals("")){
+            return false;
+        } else{
+            return true;
+        }
+    }
 
+    //Carrega dados do usuário no perfil
+    public void loadData(){
         firebaseauth = FirebaseAuth.getInstance();
         FirebaseUser user = firebaseauth.getCurrentUser();
 
@@ -184,11 +212,15 @@ public class Perfil extends AppCompatActivity
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 nome[0] = dataSnapshot.child("nome").getValue().toString();
+                sobrenome[0] = dataSnapshot.child("sobrenome").getValue().toString();
                 email[0] = dataSnapshot.child("email").getValue().toString();
+                senha[0] = dataSnapshot.child("senha").getValue().toString();
+
                 nomeSobr.setText(dataSnapshot.child("nome").getValue().toString()+" "+dataSnapshot.child("sobrenome").getValue().toString());
                 edtTel.setText(dataSnapshot.child("telefone").getValue().toString());
                 edtCel.setText(dataSnapshot.child("celular").getValue().toString());
                 edtEmail.setText(dataSnapshot.child("email").getValue().toString());
+
                 if(dataSnapshot.child("tipo").getValue().toString().equals("Cliente")){
                     rbCliente.setChecked(true);
                     rbProprietario.setChecked(false);
@@ -205,8 +237,6 @@ public class Perfil extends AppCompatActivity
                     rbMasculino.setChecked(true);
                 }
 
-                //Log.i("listUser", nome[0]);
-
                 setUserName(nome[0]);
                 setUserEmail(email[0]);
             }
@@ -216,6 +246,49 @@ public class Perfil extends AppCompatActivity
 
             }
         });
+    }
+
+    //Atualiza os dados do usuário
+    public boolean updateData(){
+        firebaseauth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseauth.getCurrentUser();
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNome(nome[0]);
+        usuario.setSobrenome(sobrenome[0]);
+        usuario.setTelefone(edtTel.getText().toString());
+        usuario.setCelular(edtCel.getText().toString());
+        usuario.setEmail(edtEmail.getText().toString());
+        usuario.setSenha(senha[0]);
+
+        //Verifica se o usuário é Cliente ou proprietário
+        if(rbCliente.isChecked()){
+            usuario.setTipo("Cliente");
+        } else{
+            usuario.setTipo("Proprietário");
+        }
+        //Verifica se o usuário é Homem ou Mulher
+        if(rbFeminino.isChecked()){
+            usuario.setSexo("Feminino");
+        } else{
+            usuario.setSexo("Masculino");
+        }
+
+        try{
+            usuarioReferencia.child(user.getUid()).setValue(usuario); //salva dados no banco de dados firebase e usa o UID como ID
+
+            return true;
+        } catch(Exception e){
+            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
